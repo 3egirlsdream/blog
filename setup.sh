96f5c4a1ee5d1dd498a67ad042acfe8700eba242
XuLezuo <xulezuo@hotmail.com>
2021-09-06 14:12:00 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ docker stop vue2docker;
 docker rm vue2docker;
 docker build -t vue2docker:$image_version .;
 docker images;
-docker run -p 80:80 -d --name vue2docker vue2docker:$image_version;
+docker run -p 80:80 -d --name vue2docker -v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v /data/nginx/log:/var/log/nginx -v /data/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf -d vue2docker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs vue2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
