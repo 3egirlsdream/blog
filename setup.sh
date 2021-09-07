2a84a305814276724139565f6dfa4328665dd429
XuLezuo <xulezuo@hotmail.com>
2021-09-07 16:31:30 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo $image_version;
 # cd vue2docker
 git pull --rebase origin master;
 docker stop vue2docker;
-docker rm vue2docker;
+docker rm vue2docker -f;
 docker build -t vue2docker:$image_version .;
 docker images;
 docker run -p 80:80 -d --name vue2docker -v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v /data/nginx/log:/var/log/nginx -v /data/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf -d vue2docker:$image_version;
