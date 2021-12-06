ccc43020f04bb70c4d5186cfaa6e5d2aacb010e7
3egirlsdream <xulezuo@hotmail.com>
2021-12-06 09:05:33 +0000

    update

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -24,7 +24,7 @@
         md="auto"
       >
         <div class="body-1 font-weight-light pt-6 pt-md-0 text-center">
-          &copy; {{ (new Date()).getFullYear() }}, Made by <v-icon>mdi-vuetify</v-icon>
+          &copy; {{ (new Date()).getFullYear() }}, Powered by <v-icon>mdi-vuetify</v-icon>
           <a
             href="https://vuetifyjs.com/en/about/meet-the-team/#company"
             class="text-decoration-none"
